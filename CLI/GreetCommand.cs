2c20165974bf455614f1819bd8e7e03b3937baa7
giodestr <destratisgiovanni2@gmail.com>
2025-03-13 13:22:46 +0100

    finalizzato la conversione

---
+++ CLI/GreetCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CommandLine.NamingConventionBinder;
+
+namespace CLI
+{
+    public class GreetCommand
+    {
+        public Command GetCommand()
+        {
+            var greetCommand = new Command("greet")
+            {
+                new Option<string>(
+                    "--name",
+                    description: "Il nome della persona"
+                ) { IsRequired = true },
+
+                new Argument<int>(
+                    "age",
+                    description: "L'età della persona"
+                ) { Arity = ArgumentArity.ExactlyOne }
+            };
+
+            greetCommand.Handler = CommandHandler.Create((string name, int age) =>
+            {
+                var greetingHandler = new GreetingHandler(); 
+                greetingHandler.Greet(name, age);
+                return Task.CompletedTask;
+            });
+
+            return greetCommand;
+        }
+    }
+
+}
